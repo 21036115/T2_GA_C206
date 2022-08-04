0db62fbd9e47f6ac3b902d26823fff396bd098b6
Eddie <106293374+21036115@users.noreply.github.com>
2022-08-04 14:30:03 +0800

    Add files via upload

---
+++ C206_CaseStudy/src/Categories.java
@@ -0,0 +1,33 @@
+package c206_Assignment;
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * 21036115, 31 Jul 2022 2:23:47 pm
+ */
+
+
+/**
+ * @author 21036115
+ *
+ */
+public class Categories {
+	
+	private String categoryName;
+	
+	public Categories(String categoryName) {
+		
+		this.categoryName = categoryName;
+	}
+
+	public String getCategoryName() {
+		return categoryName;
+	}
+
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+	
+	
+}
